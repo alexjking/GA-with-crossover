762c213c3743f948505cf451ce6c00846f30262b
Alex King <alex@alexjking.co.uk>
2014-10-06 21:59:26 +0100

    Add mutate function to Individual.

--- src/uk/co/alexjking/ga/crossover/Individual.java
+++ src/uk/co/alexjking/ga/crossover/Individual.java
@@ -10,9 +10,22 @@ import java.util.Random;
  */
 public class Individual {
 	private char[] characters;
+	private static String alphabet = "abcdefghijklmnopqrstuvwxyz ";
 	
+	/**
+	 * Initialise object with random characters.
+	 */
 	public Individual(){
 		initCharacters(28);
+	}
+	
+	/**
+	 * Initialise object with existing char array.
+	 * 
+	 * @param characters
+	 */
+	public Individual(char[] characters){
+		this.characters = characters;
 	}
 	
 	/**
@@ -23,7 +36,6 @@ public class Individual {
 		characters = new char[numCharacters];
 		
 		Random r = new Random();
-		String alphabet = "abcdefghijklmnopqrstuvwxyz ";
 		
 		for(int i=0; i<numCharacters; i++){
 			characters[i] = alphabet.charAt(r.nextInt(alphabet.length()));
@@ -36,7 +48,29 @@ public class Individual {
 
 	public void setCharacters(char[] characters) {
 		this.characters = characters;
-	}	
+	}
+	
+	/**
+	 * Returns a copy of the current object after mutating.
+	 * Each character has a 1/L chance of being mutated to another random character
+	 * where L = length of the character array.
+	 */
+	public Individual mutate(){
+		Random r = new Random();
+		
+		/* Create copy of characters array for new object */
+		char[] charactersCopy = new char[characters.length];
+		System.arraycopy(characters, 0, charactersCopy, 0, characters.length);
+		
+		/* Loop through every character mutating if necessary */
+		for(int i=0; i<charactersCopy.length; i++){
+			if(r.nextInt(charactersCopy.length) == 0){
+				/* Replace character at current position with a new random character from the alphabet */
+				charactersCopy[i] = alphabet.charAt(r.nextInt(alphabet.length()));
+			}
+		}
+		return new Individual(charactersCopy);
+	}
 	
 
 }

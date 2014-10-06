a2ce15874544a1bb430345babb1f55bddc74df4b
Alex King <alex@alexjking.co.uk>
2014-10-06 21:33:52 +0100

    Create Individual class which represents a string of random characters.

--- src/uk/co/alexjking/ga/crossover/Individual.java
+++ src/uk/co/alexjking/ga/crossover/Individual.java
@@ -1,5 +1,42 @@
 package uk.co.alexjking.ga.crossover;
 
+import java.util.Random;
+
+/**
+ * Models a string of random characters which can then mutate to model a genetic algorithm.
+ * 
+ * @author Alex King
+ *
+ */
 public class Individual {
+	private char[] characters;
+	
+	public Individual(){
+		initCharacters(28);
+	}
+	
+	/**
+	 * Initialises the char array with random lower case letters or a space.
+	 * @param numCharacters The length of the character array
+	 */
+	private void initCharacters(int numCharacters){
+		characters = new char[numCharacters];
+		
+		Random r = new Random();
+		String alphabet = "abcdefghijklmnopqrstuvwxyz ";
+		
+		for(int i=0; i<numCharacters; i++){
+			characters[i] = alphabet.charAt(r.nextInt(alphabet.length()));
+		}
+	}
+
+	public char[] getCharacters() {
+		return characters;
+	}
+
+	public void setCharacters(char[] characters) {
+		this.characters = characters;
+	}	
+	
 
 }

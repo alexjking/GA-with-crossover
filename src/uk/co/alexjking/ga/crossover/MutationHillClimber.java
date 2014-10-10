7abbee3bd7b8c3c0567af6a54360e3493f98269d
Alex King <alex@alexjking.co.uk>
2014-10-10 18:50:32 +0100

    Create a mutation hill climber to maximise the fitness of an individual through mutation.

---
+++ src/uk/co/alexjking/ga/crossover/MutationHillClimber.java
@@ -0,0 +1,38 @@
+package uk.co.alexjking.ga.crossover;
+
+
+/**
+ * Hill climber which mutates the individual to get closer to the
+ * target string.
+ * 
+ * @author Alex King
+ *
+ */
+public class MutationHillClimber {
+	
+	public static void main(String[] args){
+		MutationHillClimber hillClimber = new MutationHillClimber();
+		hillClimber.start();
+	}
+
+	/**
+	 * Starts the hill climber.
+	 */
+	public void start(){
+		Individual ind = new Individual();
+		int counter = 0;
+		
+		/* Mutate whilst individual isn't at maximum fitness. */
+		while(ind.calculateFitness() < Individual.targetString.length()){
+			Individual mutatedInd = ind.mutate();
+
+			// keep mutated individual if it has a higher fitness
+			if(mutatedInd.calculateFitness() > ind.calculateFitness()){
+				ind = mutatedInd;
+				System.out.println(counter + ": " + ind);
+			}
+			
+			counter++;
+		}
+	}
+}

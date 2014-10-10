9ec6d2c5c79447f1e18de207ff569a89fe4395ac
Alex King <alex@alexjking.co.uk>
2014-10-10 18:22:24 +0100

    Add function to calculate an individuals fitness.

--- src/uk/co/alexjking/ga/crossover/Main.java
+++ src/uk/co/alexjking/ga/crossover/Main.java
@@ -26,8 +26,26 @@ public class Main {
 	 */
 	public void printPopulation(){
 		for(int i=0; i< population.length; i++){
-			System.out.println(i + ": " + population[i]);
+			System.out.println(i + ": " + population[i] + " fit= " + calculateFitness(population[i]));
 		}
+	}
+	
+	/**
+	 * Calculate the fitness of a particular char array by comparing to 'methinks it is like a weasel'.
+	 * 
+	 * @param individual Individual we are calculating the fitness for.
+	 * @return Fitness score.
+	 */
+	private int calculateFitness(Individual individual){
+		String targetCharacters = "methinks its like a weasel";
+		char[] comparisonCharacters = individual.getCharacters();
+		int fitness = 0;
+		
+		for(int i=0; i<targetCharacters.length(); i++){
+			if(targetCharacters.charAt(i) == comparisonCharacters[i])
+				fitness++;
+		}
+		return fitness;
 	}
 	
 	

9d8fbbd5dd8516470faa3daf19f0858ed1f07d5b
Alex King <alex@alexjking.co.uk>
2014-10-10 22:00:28 +0100

    Add function to main to find average number of evaluations for the two ga's.

--- src/uk/co/alexjking/ga/crossover/Main.java
+++ src/uk/co/alexjking/ga/crossover/Main.java
@@ -6,8 +6,33 @@ public class Main {
 	
 	public static void main(String[] args){
 		Main main = new Main();
-		main.createPopulation(10);
-		main.printPopulation();
+		main.compareGA(1, 5000);
+	}
+	
+	/**
+	 * Function which runs genetic algorithm with and without crossover.
+	 * 
+	 * This prints the average number of evaluations before reaching 
+	 * maximum fitness.
+	 */
+	public void compareGA(int iterations, int populationSize){
+		GAWithoutCrossover ga = new GAWithoutCrossover();
+		GAWithCrossover gaCrossover = new GAWithCrossover();
+		
+		double gaAverage = 0.0;
+		double crossoverAverage = 0.0;
+		for(int i=0; i<iterations; i++){
+			gaAverage += ga.start(populationSize);
+			crossoverAverage += gaCrossover.start(populationSize);
+		}
+		
+		gaAverage /= iterations;
+		crossoverAverage/= iterations;
+		
+		System.out.println("GA average = " + gaAverage);
+		System.out.println("Crossover average = " + crossoverAverage);
+
+		 
 	}
 	
 	/**
